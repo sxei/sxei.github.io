48c6f8ee87f097968054466932a89cc965533508
liuxianan <937925941@qq.com>
2016-09-22 17:52:05 +0800

    增加首页自动生成功能

---
+++ index.js
@@ -0,0 +1,32 @@
+var pages = [
+	{
+		"date": "2016-01-07",
+		"title": "DEMO模板",
+		"description": "欢迎访问柳夏南的个人网站，同时这里也是柳夏南个人博客",
+		"url": "2016/01/07-demo-sample/"
+	},
+	{
+		"date": "2016-01-09",
+		"title": "HTML5 video比例拉伸示例",
+		"description": "欢迎访问柳夏南的个人网站，同时这里也是柳夏南个人博客",
+		"url": "2016/01/09-html5-video-resize/"
+	},
+	{
+		"date": "2016-03-08",
+		"title": "CSS计数器",
+		"description": null,
+		"url": "2016/03/08-css-counter/"
+	},
+	{
+		"date": "2016-08-18",
+		"title": "简单图片懒加载示例",
+		"description": "欢迎访问柳夏南的个人网站，同时这里也是柳夏南个人博客",
+		"url": "2016/08/18-simple-lazyload/"
+	},
+	{
+		"date": "2016-09-21",
+		"title": "object-fit测试",
+		"description": "欢迎访问柳夏南的个人网站，同时这里也是柳夏南个人博客",
+		"url": "2016/09/21-test-object-fit/"
+	}
+]
---
+++ make-index.js
@@ -0,0 +1,36 @@
+﻿// 文件说明：自动给所有页面生成索引
+
+var fs = require('fs');
+
+var results = [];
+var root = __dirname;
+var years = fs.readdirSync(root);
+years.forEach(function(year)
+{
+	if(fs.statSync(root+'/'+year).isFile() || !/^\d{4}$/g.test(year)) return;
+	var months = fs.readdirSync(root+'/'+year);
+	months.forEach(function(month)
+	{
+		var days = fs.readdirSync(root+'/'+year+'/'+month);
+		days.forEach(function(dayAndName)
+		{
+			var temp = /^(.+?)-(.*)$/g.exec(dayAndName);
+			var htmlPath = root+'/'+year+'/'+month+'/'+dayAndName+'/index.html';
+			var html = fs.readFileSync(htmlPath, {encoding: 'utf8'});
+			var title = /<head>[\s\S]*?<title>([\s\S]*?)<\/title>[\s\S]*?<\/head>/gim.exec(html);
+			var description = /<head>[\s\S]*?<meta.*?name="description".*?content="(.*)".*?>[\s\S]*?<\/head>/gim.exec(html);
+			results.push(
+			{
+				date: year+'-'+month+'-'+temp[1],
+				title: title?title[1]:temp[2],
+				description: description?description[1]:null,
+				url: year+'/'+month+'/'+dayAndName+'/'
+			});
+		});
+	});
+});
+fs.writeFile('./index.js', 'var pages = '+JSON.stringify(results, null, '\t'), function(err)
+{
+	if (err) throw err;
+	console.log('保存成功！');
+});
