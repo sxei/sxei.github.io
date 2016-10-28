183ede0eaa1a12fceccddbe1fc66f5cbd7a2a1cd
liuxianan <937925941@qq.com>
2016-10-28 10:40:24 +0800

    增加一些页面

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var pages = [
 		"url": "2016/01/07-demo-sample/"
 	},
 	{
+		"date": "2016-01-08",
+		"title": "css定位:absolute和relative",
+		"description": "欢迎访问柳夏南的个人网站，同时这里也是柳夏南个人博客",
+		"url": "2016/01/08-css-position/"
+	},
+	{
 		"date": "2016-01-09",
 		"title": "HTML5 video比例拉伸示例",
 		"description": "欢迎访问柳夏南的个人网站，同时这里也是柳夏南个人博客",
@@ -18,10 +24,58 @@ var pages = [
 		"url": "2016/03/08-css-counter/"
 	},
 	{
+		"date": "2016-08-17",
+		"title": "不错的404页面",
+		"description": "一个使用text-shadow实现的很不错的404页面",
+		"url": "2016/08/17-404/"
+	},
+	{
+		"date": "2016-08-17",
+		"title": "base64在线生成",
+		"description": "拖拽图片生成base64小工具",
+		"url": "2016/08/17-base64/"
+	},
+	{
+		"date": "2016-08-17",
+		"title": "在线万年历",
+		"description": "能够兼容火狐、谷歌等主流浏览器的万年历",
+		"url": "2016/08/17-wannianli/"
+	},
+	{
 		"date": "2016-08-18",
 		"title": "简单图片懒加载示例",
 		"description": "欢迎访问柳夏南的个人网站，同时这里也是柳夏南个人博客",
 		"url": "2016/08/18-simple-lazyload/"
+	},
+	{
+		"date": "2016-08-19",
+		"title": "CSS实现任意角度扇形",
+		"description": "CSS实现任意角度扇形",
+		"url": "2016/08/19-shanxing/"
+	},
+	{
+		"date": "2016-08-20",
+		"title": "div垂直水平居中的几种实现方式",
+		"description": "div垂直水平居中的几种实现方式",
+		"url": "2016/08/20-align-center/"
+	},
+	{
+		"date": "2016-08-22",
+		"title": "CSS3文字渐变",
+		"description": "CSS3文字渐变",
+		"url": "2016/08/22-css3-text-gradient/"
+	},
+	{
+		"date": "2016-08-25",
+		"title": "SVG线条动画",
+		"description": "使用SVG实现的线条动画",
+		"url": "2016/08/25-svg-stroke-animate/"
+	},
+	{
+		"date": "2016-08-26",
+		"title": "CSS3 WebFont测试",
+		"description": "自定义web字体文件示例",
+		"url": "2016/08/26-webfont/"
 	},
 	{
 		"date": "2016-09-21",
--- make-index.bat
+++ make-index.bat
@@ -1,2 +1,3 @@
+@echo off
 node make-index.js
 pause
