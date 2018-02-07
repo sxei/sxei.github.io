023cd809b29e643411a3c4c5135d18fc5b268037
liuxianan <937925941@qq.com>
2018-02-07 17:36:36 +0800

    echarts改动，还有些问题

--- 2017/05/22-echarts-demo/index.js
+++ 2017/05/22-echarts-demo/index.js
@@ -1,11 +1,13 @@
+var colors = ['#2599e4', '#FF4949', '#03C300', '#f90', '#FF00BC', '#FFD800'];
+
 //-------------------------折线图-----------------------//
 (function()
 {
-	var myChart = echarts.init(document.getElementById('echarts_demo_1'));
-	var data = generateData();
-	myChart.setOption(getOptions(data));
+	var mychart = echarts.init(document.getElementById('echarts_demo_1'));
+	var data = mockData(); // 模拟数据
+	mychart.setOption(getOptions(data));
 	// 缩放数据时触发
-	myChart.on('dataZoom', function(params)
+	mychart.on('dataZoom', function(params)
 	{
 		if(params.end - params.start < 20)
 		{
@@ -19,52 +21,56 @@
 				data.times[idx] = xei.formatDate(item, 'yyyy/MM/dd');
 			});
 		}
-		myChart.setOption({xAxis: {
+		mychart.setOption({xAxis: {
 			type: 'category',
 			boundaryGap: false,
 			data: data.times
 		}});
 	});
 
-	function generateData() {
+	function mockData() {
 		var base = +new Date(2016, 5, 23);
-		var oneDay = 24 * 3600 * 1000;
-		var timestamps = [];
-		var times = [];
-		var data = [Math.random() * 300];
-
-		for (var i = 1; i < 365; i++) {
-			var now = new Date(base += oneDay+xei.getRandom(100000));
-			timestamps.push(now);
-			times.push(xei.formatDate(now, 'yyyy/MM/dd'));
-			data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
-		}
-		return {timestamps: timestamps, times: times, data: data};
+		var x = [];
+		var y_name = ['张三', '李四', '王二麻子']; 
+		var y = []; // 二维数组
+		y_name.forEach((name, idx) => {
+			y.push([parseFloat((Math.random() * 300).toFixed(2))]);
+			// 一年的时间
+			for (var i = 0; i < 365; i++) {
+				if(idx == 0) {
+					// 每5分钟一条数据
+					var now = new Date(base += 5 * 60 * 1000);
+					x.push(xei.formatDate(now, 'yyyy-MM-dd HH:mm'));
+				}
+				y[idx].push(y[idx][i - 1] + Math.round((Math.random() - 0.5) * 20));
+			}
+			y[idx].pop(); //最后一条数据无效
+		});
+		return {x: x, y: y, y_name: y_name};
 	}
 
 	function getOptions(data) {
+		var legends = data.y_name;
+		var series = [];
 		data.y.forEach((item, idx) =>
 		{
 			var serie =
 			{
 				name: legends[idx],
-				type: view_type,
+				type: 'line',
 				data: item
 			};
-			// 如果显示气泡
-			if(this.views[i].show_bubble) {
-				serie.markPoint = {
-					data: [
-						{type: 'max', name: '最大值'},
-						{type: 'min', name: '最小值'}
-					],
-					// symbol: 'diamond',
-					symbolSize: function(val) {
-						// val表示symbol的值
-						return [(val+'').length * 20, 30];
-					}
-				};
-			}
+			serie.markPoint = {
+				data: [
+					{type: 'max', name: '最大值'},
+					{type: 'min', name: '最小值'}
+				],
+				// symbol: 'diamond',
+				symbolSize: function(val) {
+					// val表示symbol的值
+					return [(val+'').length * 20, 30];
+				}
+			};
 			series.push(serie);
 		});
 		
@@ -73,7 +79,7 @@
 				trigger: 'axis',
 				// 坐标轴指示器，坐标轴触发有效
 				axisPointer: {
-					type: view_type == 'bar' ? 'shadow' : 'line' // 默认为直线，可选为：'line' | 'shadow'
+					type: 'line' // 折线图用line，柱状图用shadow
 				}
 			},
 			legend: {
@@ -92,21 +98,23 @@
 			},
 			xAxis: {
 				type: 'category',
-				boundaryGap: view_type == 'bar', // x轴两边留白策略，默认true，折线图设置false，柱状图设置true
-				data: xdata
+				boundaryGap: false, // x轴两边留白策略，默认true，折线图设置false，柱状图设置true
+				data: data.x
 			},
 			yAxis: {
-				min: 'dataMin',
-				max: 'dataMax',
+				// min: 'dataMin',
+                // max: 'dataMax',
+                // 是否强制从零刻度开始，一般都建议开启，效果会比较好
+                // 这个比配置min和max效果要好，因为它会在dataMin的基础上再留点空白
+                scale: true,
 				type: 'value',
 				axisLabel: {
 					formatter: '{value}'
 				}
 			},
-			color: this.colors,
+			color: colors,
 			series: series
 		};
-		this.echarts_instance[i].setOption(option);
 		return option;
 	}
 
@@ -117,13 +125,13 @@
 
 
 
-(function()
+;(function()
 {
-	var myChart = echarts.init(document.getElementById('echarts_demo_1'));
+	var mychart = echarts.init(document.getElementById('echarts_demo_1'));
 	var data = generateData();
-	myChart.setOption(getOptions(data));
+	mychart.setOption(getOptions(data));
 	// 缩放数据时触发
-	myChart.on('dataZoom', function(params)
+	mychart.on('dataZoom', function(params)
 	{
 		if(params.end - params.start < 20)
 		{
@@ -137,7 +145,7 @@
 				data.times[idx] = xei.formatDate(item, 'yyyy/MM/dd');
 			});
 		}
-		myChart.setOption({xAxis: {
+		mychart.setOption({xAxis: {
 			type: 'category',
 			boundaryGap: false,
 			data: data.times
@@ -237,5 +245,5 @@
 		return option;
 	}
 
-})();
+});
 
