0cc51843c6190f12a0a735bfd8e35f16458da0fd
liuxianan <937925941@qq.com>
2018-05-02 11:36:40 +0800

    修改WebWorker

---
+++ 2018/04/28-webworker/sharedworker.js
@@ -0,0 +1,8 @@
+﻿self.onconnect = (e) => {
+	let port = e.ports[0];
+	port.addEventListener('message', (e) => {
+        console.log(e.data); // 特别注意，共享线程的console.log是看不到的
+        port.postMessage('你好，我是SharedWorker！');
+	});
+	port.start();
+}
--- 2018/04/28-webworker/webworker.js
+++ 2018/04/28-webworker/webworker.js
@@ -6,3 +6,4 @@ for(var i=0; i<10000; i++) {
 console.log('end');
 // 给主线程发消息，说我已经结束了
 postMessage({cmd: 'end'});
+console.log(self);
